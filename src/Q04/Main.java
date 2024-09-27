89b3ba563fdcea0d4fb5b16aafeb5838d421fc5a
Ana Batista <anapsb21@gmail.com>
2024-09-27 09:24:47 -0300

    resolucao exerc

--- src/Q04/Main.java
+++ src/Q04/Main.java
@@ -1,7 +1,11 @@
 package Q04;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
     }
 }
